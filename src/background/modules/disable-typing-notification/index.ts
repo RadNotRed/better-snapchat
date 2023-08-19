401cad9778c0e765c9c6760296751de3cc9113cb
rad <radnotred.1@gmail.com>
2023-08-18 22:27:04 -0400

    test: disable you are not present

--- src/background/modules/disable-typing-notification/index.ts
+++ src/background/modules/disable-typing-notification/index.ts
@@ -1,7 +1,7 @@
 import { DynamicRuleIds, PayloadNames, SettingIds } from '../../../common/constants';
 import type { RuntimeMessage } from '../../../common/types';
 
-const RULE = {
+const TYPING_NOTIFICATION_RULE = {
   id: DynamicRuleIds.BLOCK_PREVENT_TYPING_NOTIFICATION,
   priority: 1,
   action: { type: chrome.declarativeNetRequest.RuleActionType.BLOCK },
@@ -12,17 +12,31 @@ const RULE = {
   },
 };
 
+const PRESENCE_NOTIFICATION_RULE = {
+  id: DynamicRuleIds.BLOCK_YOU_ARE_NO_LONGER_PRESENT_NOTIFICATION,
+  priority: 2,
+  action: { type: chrome.declarativeNetRequest.RuleActionType.BLOCK },
+  condition: {
+    urlFilter: 'web-analytics-v2/web/events',
+    domains: ['web.snapchat.com'],
+    resourceTypes: [chrome.declarativeNetRequest.ResourceType.XMLHTTPREQUEST],
+  },
+};
+
 chrome.runtime.onMessage.addListener((message: RuntimeMessage) => {
   if (message.payloadName !== PayloadNames.SETTING_UPDATE) {
     return;
   }
 
-  if (message.settingId !== SettingIds.PREVENT_TYPING_NOTIFICATION) {
-    return;
+  if (message.settingId === SettingIds.PREVENT_TYPING_NOTIFICATION) {
+    chrome.declarativeNetRequest.updateDynamicRules({
+      removeRuleIds: [DynamicRuleIds.BLOCK_PREVENT_TYPING_NOTIFICATION],
+      addRules: [TYPING_NOTIFICATION_RULE],
+    });
+  } else if (message.settingId === SettingIds.YOU_ARE_NO_LONGER_PRESENT_NOTIFICATION) {
+    chrome.declarativeNetRequest.updateDynamicRules({
+      removeRuleIds: [DynamicRuleIds.BLOCK_YOU_ARE_NO_LONGER_PRESENT_NOTIFICATION],
+      addRules: [PRESENCE_NOTIFICATION_RULE],
+    });
   }
-
-  chrome.declarativeNetRequest.updateDynamicRules({
-    removeRuleIds: [DynamicRuleIds.BLOCK_PREVENT_TYPING_NOTIFICATION],
-    addRules: [RULE],
-  });
 });
